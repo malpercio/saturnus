225976c1b99166c4aa7fbaed7c8da3a33d7131e0
Daniel gonzález Espinoza <danielglezespinoza@gmail.com>
2017-07-12 17:53:36 -0500

    Added includes() and isNow()
    close #6

--- test/Parser.test.js
+++ test/Parser.test.js
@@ -1,5 +1,6 @@
 const should = require('should');
 const Parser = require('../lib/Parser')();
+const moment = require('moment');
 
 describe('Parser tests', () => {
   describe('constructor', () => {
@@ -49,7 +50,7 @@ describe('Parser tests', () => {
   });
 
 
-  describe('getExpression method', () => {
+  describe('getExpression()', () => {
     it('should return cron expression', () => {
       return new Promise((resolve, reject) => {
         let exp = '* * * * *';
@@ -67,6 +68,98 @@ describe('Parser tests', () => {
         exp = '1 2 3 4 5 6 2007';
         instance = new Parser(exp);
         instance.getExpression().should.equal(exp);
+        resolve();
+      });
+    });
+  });
+
+  describe('isNow()', () => {
+    it('should accept', () => {
+      return new Promise((resolve, reject) => {
+        let exp = '* * * * *';
+        let instance = new Parser(exp);
+        instance.isNow().should.be.true();
+
+        exp = '* * * * * *';
+        instance = new Parser(exp);
+        instance.isNow().should.be.true();
+
+        exp = '* * * * * *';
+        instance = new Parser(exp, {type: '6b'});
+        instance.isNow().should.be.true();
+
+        exp = '* * * * * * *';
+        instance = new Parser(exp);
+        instance.isNow().should.be.true();
+        resolve();
+      });
+    });
+    it('should reject', () => {
+      return new Promise((resolve, reject) => {
+        let minutes = moment().add(5, 'minutes').minutes(),
+          exp = minutes + ' * * * *';
+        let instance = new Parser(exp);
+        instance.isNow().should.be.true();
+
+        exp = '* ' + minutes + ' * * * *',
+        instance = new Parser(exp);
+        instance.isNow().should.be.true();
+
+        exp = minutes + ' * * * * *',
+        instance = new Parser(exp, {type: '6b'});
+        instance.isNow().should.be.true();
+
+        exp = '* ' + minutes + ' * * * * *',
+        instance = new Parser(exp);
+        instance.isNow().should.be.true();
+        resolve();
+      });
+    });
+  });
+  describe('includes()', () => {
+    it('should accept', () => {
+      return new Promise((resolve, reject) => {
+        let exp = '* * * * *',
+          date = moment().subtract(1, 'years');
+        let instance = new Parser(exp);
+        instance.includes(date).should.be.true();
+
+        exp = '* * * * * *',
+        date = moment().subtract(1, 'years');
+        instance = new Parser(exp);
+        instance.includes(date).should.be.true();
+
+        exp = '* * * * * *',
+        date = moment().subtract(1, 'years');
+        instance = new Parser(exp, {type: '6b'});
+        instance.includes(date).should.be.true();
+
+        exp = '* * * * * * *',
+        date = moment().subtract(1, 'years');
+        instance = new Parser(exp);
+        instance.includes(date).should.be.true();
+        resolve();
+      });
+    });
+    it('should reject', () => {
+      return new Promise((resolve, reject) => {
+        let minutes = moment().minutes(),
+          exp = minutes + ' * * * *',
+          date = moment().add(5, 'minutes');
+        let instance = new Parser(exp);
+        instance.includes(date).should.be.true();
+
+        exp = '* ' + minutes + ' * * * *',
+        instance = new Parser(exp);
+        instance.includes(date).should.be.true();
+
+        exp = minutes + ' * * * * *',
+        instance = new Parser(exp, {type: '6b'});
+        instance.includes(date).should.be.true();
+
+        exp = '* ' + minutes + ' * * * * *',
+        instance = new Parser(exp);
+        instance.includes(date).should.be.true();
         resolve();
       });
     });
