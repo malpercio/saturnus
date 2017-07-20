4593c90d93ad4232a559c3abc80d623e0c98a399
Daniel gonzález Espinoza <danielglezespinoza@gmail.com>
2017-07-19 21:14:27 -0500

    Added sancronos-validator 1.2.0
    Made everything sync
    
    close #32

--- test/Saturnus.test.js
+++ test/Saturnus.test.js
@@ -1,47 +1,21 @@
 const should = require('should');
-const saturnus = require('../index');
+const Saturnus = require('../index');
 
 describe('Saturnus tests', () => {
   describe('parsed object creation', () => {
     it('should create a parsed object with no options', () => {
-      return saturnus('* * * * *')
-        .then((crontab) => {
-          crontab.type.should.equal('5');
-        });
+      let crontab = new Saturnus('* * * * *');
+      crontab.type.should.equal('5');
     });
     it('should create a parsed object with forced type', () => {
-      return saturnus('* * * * * *', {type: '6b'})
-        .then((crontab) => {
-          crontab.type.should.equal('6b');
-          crontab.years.partialExpression.should.equal('*');
-          should.not.exist(crontab.seconds);
-        });
+      let crontab = new Saturnus('* * * * * *', {type: '6b'});
+      crontab.type.should.equal('6b');
+      crontab.years.partialExpression.should.equal('*');
+      should.not.exist(crontab.seconds);
     });
     it('should create a parsed object without throwing errors', () => {
-      return saturnus('* * * * * 1,2', {throw: false, type: '6b'})
-        .then((crontab) => {
-          crontab.isValid.should.equal(false);
-        });
+      let crontab = new Saturnus('* * * * * 1,2', {throw: false, type: '6b'});
+      crontab.isValid.should.equal(false);
     });
-    it('should create a parsed object with callbacks', (done) => {
-      saturnus('* * * * *', (err, crontab) => {
-        if(err){
-          return done(err);
-        }
-        crontab.isValid.should.equal(true);
-        return done();
-      });
-    });
-    it('should create a parsed object with callbacks and options', (done) => {
-      saturnus('* * * * * 1,2', {throw: false, type: '6b'}, (err, crontab) => {
-        if(err){
-          return done(err);
-        }
-        crontab.isValid.should.equal(false);
-        return done();
-      });
-    });
-
   });
-
 });
